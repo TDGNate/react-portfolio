3838f4fedae5989b455f34ff9f35127ce9c76154
Nate <itsnzte@gmail.com>
2022-10-03 12:39:38 -0700

    fixed navbar, added background

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../styles/about.css'
 
 const About = () => {
   return (
-    <div className="about-section">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ducimus voluptatibus? Magnam ullam non omnis, id blanditiis, iure animi perspiciatis nostrum quis expedita deleniti sunt. Alias unde itaque iusto sapiente.</div>
+    <div className="about-section">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ducimus voluptatibus? Magnam ullam non omnis, id blanditiis, iure animi perspiciatis nostrum quis expedita deleniti sunt. Alias unde itaque iusto sapiente.
+    </div>
   )
 }
 
