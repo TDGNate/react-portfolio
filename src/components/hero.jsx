3bd86a609236de566f6774c40d1d2a95f3c1639d
Nate <itsnzte@gmail.com>
2022-10-03 13:16:48 -0700

    updated hero section, aligned mountains and tree correctly

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,8 +4,12 @@ import "../styles/hero.css";
 const Hero = () => {
   return (
     <div className="hero" style={{ backgroundImage: "url(/images/mountains.jpg)" }}>
+
+      <img src="./svgs/flat-mountains.svg" alt="flat mountains" className="hero-smooth-mountains" />
+      
       <div className="hero-trees">
         <img src="./images/forest-trees.png" alt="" className="tree hero-tree-1" /> 
+
         <img src="./images/forest-trees.png" alt="" className="tree hero-tree-2"/> 
       </div>
       
