12ed9706ba451e730aa4585ef90ae55d16668bd0
Nate <itsnzte@gmail.com>
2022-10-02 22:17:29 -0700

    adding images and linking into the hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import Navbar from './components/navbar'
-import "./styles/main.css"
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import "./styles/main.css";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <h2>this will be the body</h2>
     </>
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+// import ForestTrees from './imgs/forest-trees.png' 
 
 const hero = () => {
   return (
     <>
-      <h1>hello</h1>
+      <div className="hero">
+        <img src={process.env.PUBLIC_URL + "svgs/flat-mountains.svg"} alt="svgs" />
+        <img src={process.env.PUBLIC_URL + "images/forest-trees.png"} alt="forest trees" />
+        <img src={process.env.PUBLIC_URL + "svgs/wave-haikei.svg"} alt="wave svg" />
+      </div>
     </>
   )
 }
