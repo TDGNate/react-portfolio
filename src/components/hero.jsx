714a7fbbc809fb0e0a69e17e351bd984a8b15ce7
Nate <itsnzte@gmail.com>
2022-10-02 21:05:40 -0700

    created a hero component, and adjusted styles in css files

---
+++ src/components/hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const hero = () => {
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  )
+}
+
+export default hero;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,18 +3,21 @@ import "../styles/navbar.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul>
-        <li className="navbar-item">
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#about-me">About Me</a>
-        </li>
-        <li className="navbar-item">
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+    <nav className="navbar-container">
+      <div className="container">
+        <a href="#about-me" className="logo">Nate</a>
+        <ul className="navbar-ul">
+          <li className="navbar-item">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#about-me">About Me</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
