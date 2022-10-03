ba9650c9bcc0b450aba2c493d9fed05a0e55db56
Nate <itsnzte@gmail.com>
2022-10-03 15:45:41 -0700

    added more global colors, and added starter styles for the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ import '../styles/about.css'
 
 const About = () => {
   return (
-    <div className="about-section">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, ducimus voluptatibus? Magnam ullam non omnis, id blanditiis, iure animi perspiciatis nostrum quis expedita deleniti sunt. Alias unde itaque iusto sapiente.
+    <div className="about-section">
+      <div className="container">
+        <h2>About Me</h2>
+        <div className="about-content">
+          <p className="about-text">Hello, I'm Christian McIlvenny but you can call me Nate! My journey began when my CyberSecurity teacher made us write our first line of code and revealed the fundamentals of computers and technology. This led to me taking a JavaScript Web Development class in college, but later I enrolled in a software engineer bootcamp at University of California, Riverside where I became a Full-Stack software engineer!</p>
+          <img src="" alt="" className="about-pic" />
+        </div>
+      </div>
     </div>
   )
 }
