5c10bde981cc1076a543b8cf694112ba44b080ac
Nate <itsnzte@gmail.com>
2022-10-03 16:17:37 -0700

    added styling for the img in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,17 @@ const About = () => {
     <div className="about-section">
       <div className="container">
         <h2>About Me</h2>
+        
         <div className="about-content">
           <p className="about-text">Hello, I'm Christian McIlvenny but you can call me Nate! My journey began when my CyberSecurity teacher made us write our first line of code and revealed the fundamentals of computers and technology. This led to me taking a JavaScript Web Development class in college, but later I enrolled in a software engineer bootcamp at University of California, Riverside where I became a Full-Stack software engineer!</p>
-          <img src="" alt="" className="about-pic" />
+
+          <div className="about-img-box">
+            <img src="./images/Nate-six-flags.jpg" alt="Nate at six flags" className="about-pic" />
+          </div>
+
         </div>
+
+        
       </div>
     </div>
   )
