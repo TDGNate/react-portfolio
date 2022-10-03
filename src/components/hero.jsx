857f600ba1c07cf6f184e509cf833f1b9a80810e
Nate <itsnzte@gmail.com>
2022-10-03 12:30:24 -0700

    fixed hero section, and fix navbar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,12 +3,12 @@ import "../styles/hero.css";
 
 const Hero = () => {
   return (
-    <div className="wrapper">
-      <div className="hero">
-        <img src={process.env.PUBLIC_URL + "svgs/flat-mountains.svg"} alt="svgs" className="hero-mountains" />
-        <img src={process.env.PUBLIC_URL + "images/forest-trees.png"} alt="forest trees" className="hero-trees"/>
-        <img src={process.env.PUBLIC_URL + "svgs/wave-haikei.svg"} alt="wave svg" className="hero-waves"/>
+    <div className="hero" style={{ backgroundImage: "url(/images/mountains.jpg)" }}>
+      <div className="hero-trees">
+        <img src="./images/forest-trees.png" alt="" className="tree hero-tree-1" /> 
+        <img src="./images/forest-trees.png" alt="" className="tree hero-tree-2"/> 
       </div>
+      
     </div>
   )
 }
