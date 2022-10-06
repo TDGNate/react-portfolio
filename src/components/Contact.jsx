0fed22b1b1d36446d924e4e13bb455a1f14929d9
Nate <itsnzte@gmail.com>
2022-10-06 14:11:12 -0700

    added more to contact section, and added a contact info above the forms

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,60 @@ const Contact = () => {
     <section id="contact" className="contact-section">
       <div className="container">
         <h2>Contact</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aperiam vel, dolorem error fuga iste officia unde impedit facilis qui aspernatur perferendis earum quisquam vitae optio adipisci sit consequuntur reprehenderit!</p>
+        <p className="contact-info">
+          Christian McIlvenny
+          <br />
+          Perris, CA
+          <br />
+          <a href="mailto:email@example.com">itsnzte@gmail.com</a>
+          
+          <br />
+          Phone: <span>951-591-3493</span>
+        </p>
+
+        <div className="contact-wrapper">
+
+          <div className="contact-wrapper-left">
+
+            <h3>Send me a message!</h3>
+
+            <form action="" className="cform">
+              {/* Name */}
+              <label className="cform-label" htmlFor="name">Name</label>
+              <input className="cform-name" type="text" id="formName" required />
+              {/* Email */}
+              <label className="cform-label" htmlFor="email">Email</label>
+              <input className="cform-email" type="email" id="formEmail" required />
+              {/* Message */}
+              <label className="cform-label" htmlFor="message">Message</label>
+              <textarea className="cform-message" type="email" id="formMessage" required />
+              {/* Button  */}
+              <button className="cform-send" type="submit">Send</button>
+              </form>
+            
+          </div>
+          
+          <div className="contact-wrapper-right">
+
+            <h3>My Socials</h3>
+
+            <form action="" className="cform">
+              {/* Name */}
+              <label className="cform-label" htmlFor="name">Name</label>
+              <input className="cform-name" type="text" id="formName" required />
+              {/* Email */}
+              <label className="cform-label" htmlFor="email">Name</label>
+              <input className="cform-email" type="email" id="formEmail" required />
+              {/* Message */}
+              <label className="cform-label" htmlFor="message">Message</label>
+              <textarea className="cform-message" type="email" id="formMessage" required />
+              {/* Button  */}
+              <button className="cform-send" type="submit">Send</button>
+              </form>
+
+          </div>
+
+        </div>
       </div>
     </section>
   )
