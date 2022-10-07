963b945a0cc42f31601f54f944e4496b8d76c44f
Nate <itsnzte@gmail.com>
2022-10-07 15:30:58 -0700

    fixed minor bug in navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -17,15 +17,23 @@ const Navbar = ({ currentPage, handlePageChange }) => {
         <a href="#hero" className="logo" onClick={() => handlePageChange('Home')}>Nate</a>
         <ul className={`navbar-ul ${isActive ? "" : "active"}`}>
 
-          <li className={currentPage === "MoreProjects" ? "navbar-item nav-active-link" : "navbar-item"}>
-            <a href="#projects"
-             onClick={() => handlePageChange('Home')}>Projects</a>
-          </li>
           <li className="navbar-item">
             <a href="#about-me"
               onClick={() => handlePageChange('Home')}
             >About Me</a>
           </li>
+
+          <li className={currentPage === "MoreProjects" ? "navbar-item nav-active-link" : "navbar-item"}>
+            <a href="#projects"
+             onClick={() => handlePageChange('Home')}>Projects</a>
+          </li>
+
+          <li className="navbar-item">
+            <a href="#skills"
+              onClick={() => handlePageChange('Home')}
+            >Skills</a>
+          </li>
+
           <li className="navbar-item">
             <a href="#contact"
               onClick={() => handlePageChange('Home')}
