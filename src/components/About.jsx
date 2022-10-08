eb1b69c57405af7dfd12d258b2125424528eb956
Nate <itsnzte@gmail.com>
2022-10-07 23:52:33 -0700

    added button in about for resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,12 @@ const About = () => {
           <h2>About Me</h2>
 
           <div className="about-content">
-            <p className="about-text revealLeft">Hello, I'm Christian McIlvenny but you can call me Nate! My journey began when my CyberSecurity teacher made us write our first line of code and revealed the fundamentals of computers and technology. <br />  <br /> This led to me taking a JavaScript Web Development class in college, but later I enrolled in a software engineer bootcamp at University of California, Riverside where I became a Full-Stack software engineer!</p>
+            <div className="about-left revealLeft">
+              <p className="about-text">Hello, I'm Christian McIlvenny but you can call me Nate! My journey began when my CyberSecurity teacher made us write our first line of code and revealed the fundamentals of computers and technology. <br />  <br /> This led to me taking a JavaScript Web Development class in college, but later I enrolled in a software engineer bootcamp at University of California, Riverside where I became a Full-Stack software engineer!</p>
+              <a href="/#" className="resume-btn" target="blank">
+                View Resume
+              </a>
+            </div>
 
             <div className="about-img-box revealRight">
               <img src={NateSixFlags} alt="Nate at six flags" className="about-pic" />
